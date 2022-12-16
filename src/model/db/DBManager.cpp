dd2c7b34a8ed39a8beb42166cf14487ba4f719ad
Nathan <nathan.faynot3@etu.univ-lorraine.fr>
2022-12-16 02:33:57 +0100

    Construction de la Db

--- src/model/db/DBManager.cpp
+++ src/model/db/DBManager.cpp
@@ -7,17 +7,42 @@ DBManager::DBManager() : dbEasyTicket(QSqlDatabase::addDatabase("QMYSQL"))
 
 }
 
-void DBManager::open(const QString& user, const QString& password)
+bool DBManager::open()
 {
-    dbEasyTicket.setHostName("127.0.0.1");
-    dbEasyTicket.setDatabaseName("EasyTicket");
-    dbEasyTicket.setUserName(user);
-    dbEasyTicket.setPassword(password);
+   QDir directoryPath;
+   QString path = directoryPath.currentPath() + "/EasyTicket.db";
+
+   this->dbEasyTicket.setDatabaseName(path);
+   this->dbEasyTicket.open();
+
+   return dbEasyTicket.isOpen();
 }
 
-bool DBManager::isOpen() const
+void createIfNotExistsDataBase()
 {
-    return dbEasyTicket.isOpen();
+   this->query.exec("CREATE TABLE IF NOT EXISTS Category (cat_id INT AUTO_INCREMENT, cat_name VARCHAR(25) NOT NULL UNIQUE, PRIMARY KEY (cat_id))");
+
+   this->query.exec("CREATE TABLE IF NOT EXISTS Message (message_id INT AUTO_INCREMENT, message_text VARCHAR(200) NOT NULL, message_date DATE DEFAULT CURRENT_DATE, Tiicket_num INT NOT NULL, PRIMARY KEY (message_id), FOREIGN KEY (ticket_num)  REFERENCES Ticket(ticket_num) ON DELETE CASCADE)");
+
+   this->query.exec("CREATE TABLE IF NOT EXISTS Ticket (ticket_num INT AUTO_INCREMENT, ticket_date Date NOT NULL DEFAULT CURRENT_DATE, user_id INT NOT NULL, cat_id INT NOT NULL, PRIMARY KEY (ticket_num), FOREIGN KEY (user_id)  REFERENCES User(user_id) ON DELETE CASCADE, FOREIGN KEY (cat_id)  REFERENCES Category(cat_id) ON DELETE CASCADE)");
+
+   this->query.exec("CREATE TABLE IF NOT EXISTS User (user_id  INT AUTO_INCREMENT, user_email VARCHAR(200) NOT NULL, user_name VARCHAR(200) NOT NULL, user_level INT NOT NULL DEFAULT 0, PRIMARY KEY (user_id))");
+
+   this->query.exec("SELECT * FROM Category;");
+
+   if(!this->query.next())
+   {
+     // qDebug() << "Pas encore créée";
+      this->query.exec("INSERT INTO Category(cat_name) VALUES('Création');");
+      this->query.exec("INSERT INTO Category(cat_name) VALUES('Visualisation');");
+      this->query.exec("INSERT INTO Category(cat_name) VALUES('Windows');");
+      this->query.exec("INSERT INTO Category(cat_name) VALUES('Réclamation');");
+
+      this->query.exec("INSERT INTO User(user_email, user_name, user_level) VALUES('admin@admin.fr', 'LeBigBoss', 2);");
+      this->query.exec("INSERT INTO User(user_email, user_name, user_level) VALUES('inge@inge.fr', 'LeBoss', 1);");
+      this->query.exec("INSERT INTO User(user_email, user_name, user_level) VALUES('tech@tech.fr', 'LApprenti', 1);");
+      this->query.exec("INSERT INTO User(user_email, user_name, user_level) VALUES('client@client.fr', 'LeRandom', 0);");
+   }
 }
 
 void DBManager::close()
