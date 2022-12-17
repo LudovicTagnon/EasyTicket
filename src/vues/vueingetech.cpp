acd6e5f8401cdf4d69fb8961a853b697203e7cbc
Nathan <nathan.faynot3@etu.univ-lorraine.fr>
2022-12-17 07:23:38 +0100

    vueingetech

--- src/vues/vueingetech.cpp
+++ src/vues/vueingetech.cpp
@@ -19,18 +19,42 @@ VueIngeTech::~VueIngeTech() {
 
 }
 
-void VueIngeTech::affichageTickets()
-{
+void VueIngeTech::affichageTickets() {
     ui->listWidgetTitreTicket->clear();
-    ui->listWidgetResumeTicket_IngeTech->clear();
     ui->listWidgetCategoryTicket->clear();
     ui->listWidgetStatusTicket->clear();
 
     QStringList qstrlistTitle;
+    QStringList qstrlistResume;
+    QStringList qstrlistCategorie;
+    QStringList qstrlistStatus;
+
     for(Ticket ticket: easyTicket.getTicketManager().getTickets()){
         qstrlistTitle << ticket.getTitle();
+
+        if(ticket.getCategory()==ALL){
+            qstrlistCategorie << (QString) "ALL";
+        }else if(ticket.getCategory()==CREATION){
+            qstrlistCategorie << (QString) "CREATION";
+        }else if(ticket.getCategory()==VISUALISATION){
+            qstrlistCategorie << (QString) "VISUALISATION";
+        }else if(ticket.getCategory()==WINDOWS){
+            qstrlistCategorie << (QString) "WINDOWS";
+        }else if(ticket.getCategory()==RECLAMATION){
+            qstrlistCategorie << (QString) "RECLAMATION";
+        }
+
+        if(ticket.getIsClosed()){
+            qstrlistStatus << "Clos";
+        } else{
+            qstrlistStatus << "En cours";
+        }
+
+
     }
     ui->listWidgetTitreTicket->addItems(qstrlistTitle);
+    ui->listWidgetCategoryTicket->addItems(qstrlistCategorie);
+    ui->listWidgetStatusTicket->addItems(qstrlistStatus);
 }
 
 void VueIngeTech::on_OuvrirButton_clicked() {
