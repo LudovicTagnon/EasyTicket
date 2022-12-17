08cfda7fde23f9f2517582966ac9a0cde270f5cb
LudovicTagnon <ludovic.tgn@outlook.com>
2022-12-17 07:16:20 +0100

    Affichage des listes selon les attributs des tickets

--- src/vues/vueclient.cpp
+++ src/vues/vueclient.cpp
@@ -24,15 +24,40 @@ VueClient::~VueClient() {
 
 void VueClient::affichageTickets() {
     ui->listWidgetNomTicket->clear();
-    ui->listWidgetResumeTicket->clear();
     ui->listWidgetCategorieTicket->clear();
     ui->listWidgetStatusResolutionTicket->clear();
 
     QStringList qstrlistTitle;
+    QStringList qstrlistResume;
+    QStringList qstrlistCategorie;
+    QStringList qstrlistStatus;
+
     for(Ticket ticket: easyTicket.getTicketManager().getTickets()){
         qstrlistTitle << ticket.getTitle();
+
+        if(ticket.getCategory()==ALL){
+            qstrlistCategorie << (QString) "ALL";
+        }else if(ticket.getCategory()==CREATION){
+            qstrlistCategorie << (QString) "CREATION";
+        }else if(ticket.getCategory()==VISUALISATION){
+            qstrlistCategorie << (QString) "VISUALISATION";
+        }else if(ticket.getCategory()==WINDOWS){
+            qstrlistCategorie << (QString) "WINDOWS";
+        }else if(ticket.getCategory()==RECLAMATION){
+            qstrlistCategorie << (QString) "RECLAMATION";
+        }
+
+        if(ticket.getIsClosed()){
+            qstrlistStatus << "Clos";
+        } else{
+            qstrlistStatus << "En cours";
+        }
+
+
     }
     ui->listWidgetNomTicket->addItems(qstrlistTitle);
+    ui->listWidgetCategorieTicket->addItems(qstrlistCategorie);
+    ui->listWidgetStatusResolutionTicket->addItems(qstrlistStatus);
 }
 
 void VueClient::on_OuvrirButton_clicked() {
@@ -52,5 +77,6 @@ void VueClient::on_EnCoursButton_clicked(){
 }
 
 void VueClient::on_RafraichirButton_clicked(){
+    easyTicket.refreshTickets();
     affichageTickets();
 }
